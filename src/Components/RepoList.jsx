fa7b9afd4068c3f24f75d757d5056fc47b4757e5
debasistripathy01 <debasistripathy04@gmail.com>
2023-03-02 12:47:10 +0530

    mock13

--- src/Components/RepoList.jsx
+++ src/Components/RepoList.jsx
@@ -63,6 +63,103 @@ export function RepoList() {
 
     const renderList = () => {
         return (
-            
+            <div className="repositories-list">
+                {paginatedRepositories.map(repository => (
+                    <div className="repository" key={repository.id}>
+                        <img
+                            className="repository-avatar"
+                            src={repository.owner.avatar_url}
+                            alt={`${repository.owner.login}'s avatar`}
+                        />
+                        <div className="repository-details">
+                            <h2 className="repository-name">
+                                <a href={repository.html_url}>{repository.name}</a>
+                            </h2>
+                            <div className="repository-info">
+                                <span className="repository-author">{repository.owner.login}</span>
+                                <span className="repository-date">{repository.updated_at}</span>
+                            </div>
+                            <p className="repository-description">{repository.description}</p>
+                            <div className="repository-counts">
+                                <span className="repository-stars">{repository.stargazers_count} stars</span>
+                                <span className="repository-forks">{repository.forks_count} forks</span>
+                                <span className="repository-issues">{repository.open_issues_count} issues</span>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+    const renderGrid = () => {
+        return (
+            <div className="repositories-grid">
+                {paginatedRepositories.map(repository => (
+                    <div className="repository" key={repository.id}>
+                        <img
+                            className="repository-avatar"
+                            src={repository.owner.avatar_url}
+                            alt={`${repository.owner.login}'s avatar`}
+                        />
+                        <div className="repository-details">
+                            <h2 className="repository-name">
+                                <a href={repository.html_url}>{repository.name}</a>
+                            </h2>
+                            <div className="repository-info">
+                                <span className="repository-author">{repository.owner.login}</span>
+                                <span className="repository-date">{repository.updated_at}</span>
+                            </div>
+                            <p className="repository-description">{repository.description}</p>
+                            <div className="repository-counts">
+                                <span className="repository-stars">{repository.stargazers_count} stars</span>
+                                <span className="repository-forks">{repository.forks_count} forks</span>
+                                <span className="repository-issues">{repository.open_issues_count} issues</span>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+    return (
+        <>
+            <div className={`app${theme}`}>
+                {/* <header className="app-header">
+                    <div className="search-container">
+                        <input
+                            type="text"
+                            className="search-input"
+                            placeholder="Search repositories"
+                            value={searchTerm}
+                            onChange={handleSearch}
+                        />
+                        <FaSearch className="search-icon" />
+                    </div>
+                    <div className="theme-toggle" onClick={handleThemeChange}>
+                        {theme === 'light' ? '🌙' : '☀️'}
+                    </div>
+                    <div className="view-toggle" onClick={handleViewTypeChange}>
+                        {viewType === 'list' ? <MdViewModule /> : <MdViewList />}
+                    </div>
+                </header> */}
+                <div className="repositories-container">
+                    {viewType === 'list' ? renderList() : renderGrid()}
+                </div>
+                <div className="pagination-container">
+                    <Pagination
+                        currentPage={currentPage}
+                        pageCount={pageCount}
+                        onPageChange={handlePageChange}
+                    />
+                </div>
+                <footer className="app-footer">
+                    <p>
+                        Modeled by <a href="https://github.com/debasistripathy01">Debasis Tripathy (Thor)</a>
+                    </p>
+                </footer>
+            </div>
+
+        </>
     )
 }
